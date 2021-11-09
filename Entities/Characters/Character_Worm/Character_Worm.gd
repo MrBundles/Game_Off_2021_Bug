7071dd6e3561b1b95adac06546f7394b0dc4d8b9
MrBundles <klwagne1@gmail.com>
2021-11-09 17:42:53 -0500

    Added script to worm character that will automatically generate rigidbody segments and then add connecting joints between each. I'd like this to form the physical presence of the worm and then paint the worm over top of it.

--- Entities/Characters/Character_Worm/Character_Worm.gd
+++ Entities/Characters/Character_Worm/Character_Worm.gd
@@ -1,4 +1,5 @@
-extends Line2D
+tool
+extends Node2D
 
 # references -----------------------------------------------------------------------------------------------------------
 
@@ -7,6 +8,15 @@ extends Line2D
 
 
 # variables ------------------------------------------------------------------------------------------------------------
+export var generate = false
+
+# worm parameters
+export(float, 1, 256) var length = 10.0
+export(int, 2, 256) var segment_qty = 10
+
+# scenes for instancing
+export(PackedScene) var worm_segment
+export(PackedScene) var worm_joint
 
 
 
@@ -17,6 +27,9 @@ func _ready():
 	# initialize setgets
 	
 	# initialize variables
+	
+	if generate:
+		generate_segments()
 	pass
 
 
@@ -32,7 +45,38 @@ func _get_configuration_warning():
 
 
 # helper functions ------------------------------------------------------------------------------------------------------
+func clear_segments():
+	for child in $Segments.get_children():
+		child.queue_free()
 
+
+func generate_segments():
+	# clear all current segments
+	clear_segments()
+	
+	# calculate individual segment length
+	var segment_length = length / segment_qty
+	
+	# first add a segment
+	var worm_segment_instance : RigidBody2D = worm_segment.instance()
+	$Segments.add_child(worm_segment_instance)
+	
+	# then loop over all remaining segments and add them as well as a joint between each
+	for i in range(1, segment_qty):
+		# add joint
+		var worm_joint_instance : PinJoint2D = worm_joint.instance()
+		worm_joint_instance.position.x = i * segment_length - segment_length / 2	# This should place the joint in between the previous two segments
+		$Segments.add_child(worm_joint_instance)
+		
+		# add segment
+		worm_segment_instance = worm_segment.instance()
+		worm_segment_instance.position.x = i * segment_length
+		$Segments.add_child(worm_segment_instance)
+		
+		# connect joints between segments
+		worm_joint_instance.node_a = worm_joint_instance.get_path_to($Segments.get_child(2 * i - 2))
+		worm_joint_instance.node_b = worm_joint_instance.get_path_to($Segments.get_child(2 * i))
+		print("node a: " + str(worm_joint_instance.node_a) + "    node b: " + str(worm_joint_instance.node_b))
 
 
 # set/get functions ------------------------------------------------------------------------------------------------------
