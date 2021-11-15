bdf4bc1339674dcb1a2d379ff14a03737c1c5e51
MrBundles <klwagne1@gmail.com>
2021-11-15 16:52:59 -0500

    Added squash and stretch to worm segments during movement. The farther the segments stretch, the smaller the segment radius.

--- Entities/Characters/Character_Worm/Character_Worm_Handle/Character_Worm_Handle.gd
+++ Entities/Characters/Character_Worm/Character_Worm_Handle/Character_Worm_Handle.gd
@@ -10,6 +10,8 @@ extends RigidBody2D
 var handle_type = GVM.HANDLE_TYPES.null
 export var move_force = 20.0
 export var radius = 16.0 setget set_radius
+var bodies = []
+var bodies_grabbed = []
 
 
 # main functions -------------------------------------------------------------------------------------------------------
@@ -21,12 +23,11 @@ func _ready():
 	
 	# initialize variables
 	pass
-	
-	update_joint()
 
 
 func _process(delta):
 	get_input()
+	update_radius()
 
 
 func _get_configuration_warning():
@@ -40,16 +41,72 @@ func _get_configuration_warning():
 func get_input():
 	match handle_type:
 		GVM.HANDLE_TYPES.front:
+			if Input.is_action_just_pressed("left_click"):
+				clear_pins()
+			
 			if Input.is_action_pressed("left_click"):
-					apply_central_impulse(global_position.direction_to(get_global_mouse_position()).normalized() * move_force)
+				apply_central_impulse(global_position.direction_to(get_global_mouse_position()).normalized() * move_force)
+				
+			elif bodies.size() > 0:
+				for b in bodies:
+					add_body_pin(b)
 			
 		GVM.HANDLE_TYPES.back:
+			if Input.is_action_just_pressed("right_click"):
+				clear_pins()
+			
 			if Input.is_action_pressed("right_click"):
-					apply_central_impulse(global_position.direction_to(get_global_mouse_position()).normalized() * move_force)
+				apply_central_impulse(global_position.direction_to(get_global_mouse_position()).normalized() * move_force)
+			
+			elif bodies.size() > 0:
+				for b in bodies:
+					add_body_pin(b)
 
 
-func update_joint():
-	$PinJoint2D.node_b = $PinJoint2D.get_path_to(get_parent())
+func update_radius():
+	match handle_type:
+		GVM.HANDLE_TYPES.front:
+			self.radius = get_parent().radius
+			
+		GVM.HANDLE_TYPES.back:
+			self.radius = get_parent().get_parent().radius
+
+
+func clear_pins():
+	for child in $PinJoints.get_children():
+		child.queue_free()
+	
+	bodies.clear()
+	bodies_grabbed.clear()
+
+
+func add_body_pin(body):
+	if body in bodies_grabbed:
+		return
+	
+	# add a pin
+	var pin_instance = PinJoint2D.new()
+	$PinJoints.add_child(pin_instance)
+	pin_instance.node_a = pin_instance.get_path_to(self)
+	pin_instance.node_b = pin_instance.get_path_to(body)
+	
+	bodies_grabbed.append(body)
+
+
+func add_tilemap_pin():
+	# add a static body to attach to
+	var static_instance = StaticBody2D.new()
+	var static_shape_instance = CollisionShape2D.new()
+	static_shape_instance.shape = CircleShape2D.new()
+	static_shape_instance.shape.radius = 16.0
+	static_instance.add_child(static_shape_instance)
+	$PinJoints.add_child(static_instance)
+	
+	# add a pin
+	var pin_instance = PinJoint2D.new()
+	$PinJoints.add_child(pin_instance)
+	pin_instance.node_a = pin_instance.get_path_to(self)
+	pin_instance.node_b = pin_instance.get_path_to(static_instance)
 
 
 # set/get functions ------------------------------------------------------------------------------------------------------
@@ -58,8 +115,17 @@ func set_radius(new_val):
 	
 	if has_node("CollisionShape2D"):
 		$CollisionShape2D.shape.radius = radius
+	
+	if has_node("Area2D"):
+		$Area2D/CollisionShape2D.shape.radius = radius + 6
 
 
-# signal functions -------------------------------------------------------------------------------------------------------
+# signal functions ------------------------------------------------------------------------------------------------------
+func _on_Area2D_body_entered(body):
+	if not body in bodies and body.is_in_group("grabbable"):
+		bodies.append(body)
 
 
+func _on_Area2D_body_exited(body):
+	if body in bodies:
+		bodies.remove(bodies.find(body))
--- Entities/Characters/Character_Worm/Character_Worm_Segment/Character_Worm_Segment.gd
+++ Entities/Characters/Character_Worm/Character_Worm_Segment/Character_Worm_Segment.gd
@@ -11,10 +11,14 @@ var segment_type = GVM.SEGMENT_TYPES.null
 export var move_force = 20.0
 export(String, FILE) var worm_segment_path
 export(String, FILE) var worm_handle_path
-export var radius = 16.0 setget set_radius
+export var radius_stretch = 8.0
+export var radius_rest = 16.0 setget set_radius_rest
+var radius = 16.0 setget set_radius
 export var length_stretch = 150.0 setget set_length_stretch
 export var length_rest = 50.0 setget set_length_rest
 var length = 50.0 setget set_length
+
+var contact_positions = []
 
 
 # main functions -------------------------------------------------------------------------------------------------------
@@ -29,7 +33,6 @@ func _ready():
 	# initialize variables
 	if segment_types:
 		segment_type = segment_types[0]
-		print(segment_type)
 	
 	if not Engine.editor_hint:
 		generate_segments()
@@ -38,29 +41,18 @@ func _ready():
 
 func _process(delta):
 	update_collision_shape()
-	if not Engine.editor_hint:
-		match segment_type:
-			GVM.SEGMENT_TYPES.front:
-				pass
-				
-			GVM.SEGMENT_TYPES.mid_front:
-				pass
-			
-			GVM.SEGMENT_TYPES.mid_back:
-				pass
-			
-			GVM.SEGMENT_TYPES.back:
-				pass
+	get_input()
 
 
 func _integrate_forces(state):
-#	for i in range(state.get_contact_count()):
-#		contact_positions.append(state.get_contact_local_position(i))
+	for i in range(state.get_contact_count()):
+		contact_positions.append(state.get_contact_local_position(i))
 	
 	update()
 
 
 func _draw():
+#	print(contact_positions)
 	draw_set_transform_matrix(transform.inverse())
 	
 #	for pos in contact_positions:
@@ -75,6 +67,23 @@ func _get_configuration_warning():
 
 
 # helper functions ------------------------------------------------------------------------------------------------------
+func get_input():
+	if Engine.editor_hint:
+		return
+	
+	if segment_type == GVM.SEGMENT_TYPES.front or segment_type == GVM.SEGMENT_TYPES.mid_front:
+		if Input.is_action_pressed("left_click"):
+			$DampedSpringJoint2D.stiffness = 32
+		else:
+			$DampedSpringJoint2D.stiffness = 64
+		
+	if segment_type == GVM.SEGMENT_TYPES.back or segment_type == GVM.SEGMENT_TYPES.mid_back:
+		if Input.is_action_pressed("right_click"):
+			$DampedSpringJoint2D.stiffness = 32
+		else:
+			$DampedSpringJoint2D.stiffness = 64
+
+
 func generate_segments():
 	if not worm_segment_path:
 		return
@@ -89,8 +98,6 @@ func generate_segments():
 		$ChildSegmentPosition.add_child(worm_segment_instance)											# add worm instance as a child of ChildSegmentPosition
 		$GrooveJoint2D.node_b = $GrooveJoint2D.get_path_to(worm_segment_instance)						# update node_b of GrooveJoint
 		$DampedSpringJoint2D.node_b = $DampedSpringJoint2D.get_path_to(worm_segment_instance)			# update node_b of SpringJoint
-#	else:
-#		move = true
 
 
 func add_handle():
@@ -107,25 +114,47 @@ func add_handle():
 			GVM.SEGMENT_TYPES.front:
 				worm_handle_instance.handle_type = GVM.HANDLE_TYPES.front
 				worm_handle_instance.position = Vector2(0,0)
+				add_child(worm_handle_instance)
+				
+				# pin handle
+				var pin_joint_instance = PinJoint2D.new()
+				pin_joint_instance.position = Vector2(0,0)
+				add_child(pin_joint_instance)
+				pin_joint_instance.node_a = pin_joint_instance.get_path_to(self)
+				pin_joint_instance.node_b = pin_joint_instance.get_path_to(worm_handle_instance)
+				
 			GVM.SEGMENT_TYPES.back:
 				worm_handle_instance.handle_type = GVM.HANDLE_TYPES.back
-				worm_handle_instance.position = $ChildSegmentPosition.position
-		
-		# add handle as child node
-		add_child(worm_handle_instance)
+				worm_handle_instance.position = Vector2(0,0)
+				$ChildSegmentPosition.add_child(worm_handle_instance)
+				$GrooveJoint2D.node_b = $GrooveJoint2D.get_path_to(worm_handle_instance)						# update node_b of GrooveJoint
+				$DampedSpringJoint2D.node_b = $DampedSpringJoint2D.get_path_to(worm_handle_instance)			# update node_b of SpringJoint
 
 
 func update_collision_shape():
+	# update length of collisionshape
 	if has_node("CollisionShape2D") and has_node("ChildSegmentPosition"):
-		if $ChildSegmentPosition.get_child_count() < 1:
-			return
+		var stretch_dist = length
 		
-		var stretch_dist = global_position.distance_to($ChildSegmentPosition.get_child(0).global_position)
+		if $ChildSegmentPosition.get_child_count() > 0:
+			stretch_dist = global_position.distance_to($ChildSegmentPosition.get_child(0).global_position)
+			
 		$CollisionShape2D.shape.height = stretch_dist
 		$CollisionShape2D.position.x = stretch_dist / 2
+		
+		#update radius of collisionshape
+		var max_stretch_dist = length_rest * 2
+		print(stretch_dist / max_stretch_dist)
+		self.radius = lerp(radius_rest, radius_stretch, clamp(stretch_dist / max_stretch_dist, 0.0, 1.0))
 
 
 # set/get functions ------------------------------------------------------------------------------------------------------
+func set_radius_rest(new_val):
+	radius_rest = new_val
+	
+	self.radius = radius_rest
+
+
 func set_radius(new_val):
 	radius = new_val
 	
@@ -146,7 +175,10 @@ func set_length_rest(new_val):
 	self.length = length_rest
 
 
-func set_length(new_val):	
+func set_length(new_val):
+	if length == new_val:
+		return
+		
 	length = clamp(new_val, 1.0, length_stretch)
 	
 	if has_node("GrooveJoint2D"):
