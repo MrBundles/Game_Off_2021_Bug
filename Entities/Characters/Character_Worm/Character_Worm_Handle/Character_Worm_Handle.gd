96cffd6e3d01817585bc40b7efde735c8662ce76
MrBundles <klwagne1@gmail.com>
2021-11-15 12:33:10 -0500

    Added a new node Character_Worm_Handle which is attached to the first and last worm segments. This helps to way down the ends of the segments so they don't float. This is also the node that handles the move forces when clicking the mouse.

--- Entities/Characters/Character_Worm/Character_Worm_Handle/Character_Worm_Handle.gd
+++ Entities/Characters/Character_Worm/Character_Worm_Handle/Character_Worm_Handle.gd
@@ -1,9 +1,4 @@
-tool
-extends Node
-
-# enums
-enum SEGMENT_TYPES {null, front, mid_front, mid_back, back}
-enum HANDLE_TYPES {null, front, back}
+extends RigidBody2D
 
 # references -----------------------------------------------------------------------------------------------------------
 
@@ -12,10 +7,9 @@ enum HANDLE_TYPES {null, front, back}
 
 
 # variables ------------------------------------------------------------------------------------------------------------
-
-# theme management variables
-export(Array, Theme) var theme_array = []
-var current_theme_id = 0
+var handle_type = GVM.HANDLE_TYPES.null
+export var move_force = 20.0
+export var radius = 16.0 setget set_radius
 
 
 # main functions -------------------------------------------------------------------------------------------------------
@@ -23,13 +17,16 @@ func _ready():
 	# connect signals
 	
 	# initialize setgets
+	self.radius = radius
 	
 	# initialize variables
 	pass
+	
+	update_joint()
 
 
 func _process(delta):
-	pass
+	get_input()
 
 
 func _get_configuration_warning():
@@ -40,11 +37,27 @@ func _get_configuration_warning():
 
 
 # helper functions ------------------------------------------------------------------------------------------------------
+func get_input():
+	match handle_type:
+		GVM.HANDLE_TYPES.front:
+			if Input.is_action_pressed("left_click"):
+					apply_central_impulse(global_position.direction_to(get_global_mouse_position()).normalized() * move_force)
+			
+		GVM.HANDLE_TYPES.back:
+			if Input.is_action_pressed("right_click"):
+					apply_central_impulse(global_position.direction_to(get_global_mouse_position()).normalized() * move_force)
 
+
+func update_joint():
+	$PinJoint2D.node_b = $PinJoint2D.get_path_to(get_parent())
 
 
 # set/get functions ------------------------------------------------------------------------------------------------------
-
+func set_radius(new_val):
+	radius = new_val
+	
+	if has_node("CollisionShape2D"):
+		$CollisionShape2D.shape.radius = radius
 
 
 # signal functions -------------------------------------------------------------------------------------------------------
--- Entities/Characters/Character_Worm/Character_Worm_Segment/Character_Worm_Segment.gd
+++ Entities/Characters/Character_Worm/Character_Worm_Segment/Character_Worm_Segment.gd
@@ -7,14 +7,14 @@ extends RigidBody2D
 
 # variables ------------------------------------------------------------------------------------------------------------
 export(Array, GVM.SEGMENT_TYPES) var segment_types = []
-export(String, FILE) var worm_segment_path
-export(int, 1, 256) var segment_qty = 5
-var segment_mid_cutoff = 2
 var segment_type = GVM.SEGMENT_TYPES.null
+export var move_force = 20.0
+export(String, FILE) var worm_segment_path
+export(String, FILE) var worm_handle_path
 export var radius = 16.0 setget set_radius
-export var length = 50.0 setget set_length
-export var length_max = 150.0 setget set_length_max
-export var length_min = 25.0
+export var length_stretch = 150.0 setget set_length_stretch
+export var length_rest = 50.0 setget set_length_rest
+var length = 50.0 setget set_length
 
 
 # main functions -------------------------------------------------------------------------------------------------------
@@ -23,25 +23,34 @@ func _ready():
 	
 	# initialize setgets
 	self.radius = radius
-	self.length = length
-	self.length_max = length_max
+	self.length_stretch = length_stretch
+	self.length_rest = length_rest
 	
 	# initialize variables
-	if segment_qty > segment_mid_cutoff:
-		segment_type = GVM.SEGMENT_TYPES.front
-	else:
-		segment_type = GVM.SEGMENT_TYPES.back
-	print(segment_type)
+	if segment_types:
+		segment_type = segment_types[0]
+		print(segment_type)
 	
 	if not Engine.editor_hint:
-		generate_segments(segment_qty)
+		generate_segments()
+		add_handle()
 
 
 func _process(delta):
 	update_collision_shape()
-	if not Engine.editor_hint and Input.is_action_pressed("left_click"):
-#		apply_central_impulse(global_position.direction_to(get_global_mouse_position()).normalized() * 10.0)
-		apply_impulse(Vector2(0,0), global_position.direction_to(get_global_mouse_position()).normalized() * 20.0)
+	if not Engine.editor_hint:
+		match segment_type:
+			GVM.SEGMENT_TYPES.front:
+				pass
+				
+			GVM.SEGMENT_TYPES.mid_front:
+				pass
+			
+			GVM.SEGMENT_TYPES.mid_back:
+				pass
+			
+			GVM.SEGMENT_TYPES.back:
+				pass
 
 
 func _integrate_forces(state):
@@ -66,23 +75,44 @@ func _get_configuration_warning():
 
 
 # helper functions ------------------------------------------------------------------------------------------------------
-func generate_segments(_segment_qty):
+func generate_segments():
 	if not worm_segment_path:
 		return
 	
-	if _segment_qty > 1:
+	if segment_types and segment_types.size() > 1:
 		var worm_segment_instance = load(worm_segment_path).instance()									# create worm instance
-		worm_segment_instance.length = length															# update worm instance variables
-		worm_segment_instance.length_max = length_max
-		worm_segment_instance.length_min = length_min
+		worm_segment_instance.length_stretch = length_stretch
+		worm_segment_instance.length_rest = length_rest
 		worm_segment_instance.radius = radius
-		worm_segment_instance.segment_qty = _segment_qty - 1
-		worm_segment_instance.segment_mid_cutoff = segment_mid_cutoff
+		segment_types.remove(0)
+		worm_segment_instance.segment_types = segment_types
 		$ChildSegmentPosition.add_child(worm_segment_instance)											# add worm instance as a child of ChildSegmentPosition
 		$GrooveJoint2D.node_b = $GrooveJoint2D.get_path_to(worm_segment_instance)						# update node_b of GrooveJoint
 		$DampedSpringJoint2D.node_b = $DampedSpringJoint2D.get_path_to(worm_segment_instance)			# update node_b of SpringJoint
 #	else:
 #		move = true
+
+
+func add_handle():
+	if not worm_handle_path:
+		return
+	
+	if segment_type == GVM.SEGMENT_TYPES.front or segment_type == GVM.SEGMENT_TYPES.back:
+		var worm_handle_instance = load(worm_handle_path).instance()
+		worm_handle_instance.radius = radius
+		worm_handle_instance.move_force = move_force
+		
+		# set handle position
+		match segment_type:
+			GVM.SEGMENT_TYPES.front:
+				worm_handle_instance.handle_type = GVM.HANDLE_TYPES.front
+				worm_handle_instance.position = Vector2(0,0)
+			GVM.SEGMENT_TYPES.back:
+				worm_handle_instance.handle_type = GVM.HANDLE_TYPES.back
+				worm_handle_instance.position = $ChildSegmentPosition.position
+		
+		# add handle as child node
+		add_child(worm_handle_instance)
 
 
 func update_collision_shape():
@@ -92,6 +122,7 @@ func update_collision_shape():
 		
 		var stretch_dist = global_position.distance_to($ChildSegmentPosition.get_child(0).global_position)
 		$CollisionShape2D.shape.height = stretch_dist
+		$CollisionShape2D.position.x = stretch_dist / 2
 
 
 # set/get functions ------------------------------------------------------------------------------------------------------
@@ -102,15 +133,21 @@ func set_radius(new_val):
 		$CollisionShape2D.shape.radius = radius
 
 
-func set_length(new_val):
-	if new_val > length_max:
-		return
+func set_length_stretch(new_val):
+	length_stretch = new_val
 	
-	length = new_val
+	if has_node("GrooveJoint2D"):
+		$GrooveJoint2D.length = length_stretch
+
+
+func set_length_rest(new_val):
+	length_rest = new_val
 	
-	if has_node("CollisionShape2D"):
-#		$CollisionShape2D.shape.height = length
-		$CollisionShape2D.position.x = length / 2
+	self.length = length_rest
+
+
+func set_length(new_val):	
+	length = clamp(new_val, 1.0, length_stretch)
 	
 	if has_node("GrooveJoint2D"):
 		$GrooveJoint2D.initial_offset = length
@@ -122,11 +159,5 @@ func set_length(new_val):
 	if has_node("ChildSegmentPosition"):
 		$ChildSegmentPosition.position.x = length
 
-
-func set_length_max(new_val):
-	length_max = new_val
-	
-	if has_node("GrooveJoint2D"):
-		$GrooveJoint2D.length = length_max
 
 # signal functions -------------------------------------------------------------------------------------------------------
